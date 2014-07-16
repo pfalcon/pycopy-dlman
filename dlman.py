58cd3d8fe731373369833c0b1447f0f74cbbec6a
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-07-16 23:57:47 +0300

    Try differentiate transient and permanent errors.

--- dlman.py
+++ dlman.py
@@ -5,12 +5,16 @@ import os.path
 
 DIR = "urls"
 DONE_SUBDIR = "_done_"
+ERROR_SUBDIR = "_error_"
 WGET = "wget --directory-prefix=dl/ --limit=1.1M --trust-server-names -c -i "
 LOG_TICKS = 5 * 60
 
 from config import *
 
+TRANSIENT_ERRORS = (4, 7, 8)
+
 done_full_path = DIR + "/" + DONE_SUBDIR + "/"
+error_full_path = DIR + "/" + ERROR_SUBDIR + "/"
 
 def download(f):
     path = DIR + "/" + f
@@ -19,16 +23,22 @@ def download(f):
         rc = os.system(WGET + path)
         if rc == 0:
             break
-        print("Error code: %d, sleeping before retry..." % (rc // 256))
+        print("Error code: %d" % (rc // 256))
+        if rc not in TRANSIENT_ERRORS:
+            break
+        print("Sleeping before retry...")
         sys.stdout.flush()
         time.sleep(3)
-    os.rename(path, done_full_path + f)
+    if rc == 0:
+        os.rename(path, done_full_path + f)
+    else:
+        os.rename(path, error_full_path + f)
 
 def loop():
     while True:
         t1 = time.time()
         files = os.listdir(DIR)
-        files = filter(lambda f: f != DONE_SUBDIR, files)
+        files = filter(lambda f: f[0] != "_", files)
         for f in files:
             print(f)
             download(f)
@@ -40,6 +50,8 @@ def loop():
 def main():
     if not os.path.isdir(done_full_path):
         os.makedirs(done_full_path)
+    if not os.path.isdir(error_full_path):
+        os.makedirs(error_full_path)
     loop()
 
 main()
